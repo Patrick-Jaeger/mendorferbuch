e05da48eb26ddebc477ce83a3f2249bec09aed56
Patrick-Jaeger <45011971+Patrick-Jaeger@users.noreply.github.com>
2026-03-20 14:28:21 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
+// HashRouter sorgt dafür, dass GitHub Pages die Routen korrekt lädt
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
